ef00672724d3607aed22cc3e66a3e516c1ded0c5
Neerajsainiwins <neerajsainiwins@gmail.com>
2021-05-26 16:04:38 +0530

    delete files

--- React/chime/app/meeting/timer.d.ts
+++
@@ -1,4 +0,0 @@
-export default class Timer {
-    initialize(endDateTime: string, beforeMeetingEndTime: number, callback: any): void;
-    convertUTCDateTimeToUserSepcificDateTime(endDate: string, timeZoneOffset: number): Date;
-}
