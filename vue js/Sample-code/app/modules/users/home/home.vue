fbd887e3e1a2672c43a9931d7e469642878d688f
Neeraj <neerajsainiwins@gmail.com>
2022-08-08 09:42:10 +0530

    Latest code snippets for SQL

--- vue js/Sample-code/app/modules/users/home/home.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <p>home works</p>
-</template>
--- vue js/Sample-code/app/shared/layouts/index.js
+++
@@ -1,3 +0,0 @@
-export { default as AppHeader } from './headers/app-header/app-header.vue';
-export { default as AppFooter } from './footers/app-footer/app-footer.vue';
-
